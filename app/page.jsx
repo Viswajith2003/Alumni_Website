34d2a76c7a393f2c1841df68351061be6026c09d
Viswajith2003 <viswajithviswa715@gmail.com>
2024-03-12 18:36:53 +0530

    inital commit

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="font-bold text-4xl text-center">
+        Alumni Mangement system
+      </h1>
+    </div>
+  );
+}
