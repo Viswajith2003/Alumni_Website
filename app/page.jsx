422ec9d027190e33d70a77e720f7ff5e8ddc2109
Viswajith2003 <viswajithviswa715@gmail.com>
2024-04-03 09:24:57 +0530

    about page starting

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,14 @@
 import Navbar from "../components/Navbar/navbar";
 import Hero from "../components/Hero/hero";
+import About from "../components/About/about";
+
 
 export default function Home() {
   return (
     <main>
-      <Navbar />
-      <Hero />
+      {/* <Navbar />
+      <Hero /> */}
+      <About/>
     </main>
   );
 }
---
+++ components/About/about.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div>
+      <div>
+        <div>
+          <h1>About GECP</h1>
+          <p>
+            Please click the Read more button to know more details about
+            Government Engineering College Sreekrishnapuram, Palakkad
+          </p>
+          <button>Read More</button>
+        </div>
+        <div>pictures1</div>
+      </div>
+      <div>
+        <h1>About this website</h1>
+        <div>picture2</div>
+        <div>
+          <h1>About this website</h1>
+          <p>
+            This Web Application Project is called Web Based Alumni Tracking
+            System. This is our project during Final Year Project. It is
+            originally written in next js and firebase. This simple alumns
+            tracking web app is built for Government Engineering College
+            Sreekrishnapuram, Palakkad. The system has an online form for she
+            graduated mident of the said University to track their employment
+            status This website has a admin panel and in the admin panel there
+            are so many features Alumni can't use the service of this website
+            until admin will not verify them. The admin panel has the list of
+            students, employed, self-employed, and unemployed.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
