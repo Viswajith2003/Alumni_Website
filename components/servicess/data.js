10eaac5216d0ea08b65b69a71b4ede5734bb3bd7
Viswajith2003 <viswajithviswa715@gmail.com>
2024-04-15 16:12:33 +0530

    mapped

--- components/servicess/data.js
+++ components/servicess/data.js
@@ -1,4 +1,5 @@
 import { RiGraduationCapFill } from "react-icons/ri";
+import { IoNotificationsSharp } from "react-icons/io5";
 
 const data=[
     {
@@ -7,28 +8,28 @@ const data=[
         para: "You can get the latest update of college events"
     },
     {
-        icon: RiGraduationCapFill,
-        name: "College Events",
-        para: "You can get the latest update of college events"
+        icon: IoNotificationsSharp,
+        name: "Job notification",
+        para: "You can find the job notification"
     },
     {
         icon: RiGraduationCapFill,
-        name: "College Events",
-        para: "You can get the latest update of college events"
+        name: "Connect with Friends",
+        para: "You can able to connect with your college friends over here"
     },
     {
         icon: RiGraduationCapFill,
-        name: "College Events",
-        para: "You can get the latest update of college events"
+        name: "Admin Panal",
+        para: "The website provide an admin panel."
     },
     {
         icon: RiGraduationCapFill,
-        name: "College Events",
-        para: "You can get the latest update of college events"
+        name: "Only Verified Alumni",
+        para: "In this website only verified user are allowed. verificarion is done by admin."
     },
     {
         icon: RiGraduationCapFill,
-        name: "College Events",
-        para: "You can get the latest update of college events"
+        name: "Ideas",
+        para: "Alumni can share own views about new technology to others"
     },
 ]
