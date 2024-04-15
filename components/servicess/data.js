aea69d2fc4d736b5004f273f43d31f32f10a2c72
Viswajith2003 <viswajithviswa715@gmail.com>
2024-04-15 22:04:10 +0530

    icons changed

--- components/servicess/data.js
+++ components/servicess/data.js
@@ -2,40 +2,40 @@ import { RiGraduationCapFill } from "react-icons/ri";
 import { IoNotificationsSharp } from "react-icons/io5";
 import { FaUserFriends } from "react-icons/fa";
 import { RiAdminFill } from "react-icons/ri";
-import { FaCalendarCheck } from "react-icons/fa6";
+import { MdLibraryAddCheck } from "react-icons/md";
 import { TbBulbFilled } from "react-icons/tb";
 
-const data=[
-    {
-        icon: RiGraduationCapFill,
-        name: "College Events",
-        para: "You can get the latest update of college events"
-    },
-    {
-        icon: IoNotificationsSharp,
-        name: "Job notification",
-        para: "You can find the job notification"
-    },
-    {
-        icon: FaUserFriends,
-        name: "Connect with Friends",
-        para: "You can able to connect with your college friends over here"
-    },
-    {
-        icon: RiAdminFill,
-        name: "Admin Panal",
-        para: "The website provide an admin panel."
-    },
-    {
-        icon: FaCalendarCheck,
-        name: "Only Verified Alumni",
-        para: "In this website only verified user are allowed. verificarion is done by admin."
-    },
-    {
-        icon: TbBulbFilled,
-        name: "Ideas",
-        para: "Alumni can share own views about new technology to others"
-    },
-]
+const data = [
+  {
+    icon: RiGraduationCapFill,
+    name: "College Events",
+    para: "You can get the latest update of college events",
+  },
+  {
+    icon: IoNotificationsSharp,
+    name: "Job notification",
+    para: "You can find the job notification",
+  },
+  {
+    icon: FaUserFriends,
+    name: "Connect with Friends",
+    para: "You can able to connect with your college friends over here",
+  },
+  {
+    icon: RiAdminFill,
+    name: "Admin Panal",
+    para: "The website provide an admin panel.",
+  },
+  {
+    icon: MdLibraryAddCheck,
+    name: "Only Verified Alumni",
+    para: "In this website only verified user are allowed. verificarion is done by admin.",
+  },
+  {
+    icon: TbBulbFilled,
+    name: "Ideas",
+    para: "Alumni can share own views about new technology to others",
+  },
+];
 
-export default data
+export default data;
