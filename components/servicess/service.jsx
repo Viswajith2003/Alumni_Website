01d53772befecd035477119e731377ac9c1bd2c0
Viswajith2003 <viswajithviswa715@gmail.com>
2024-04-15 21:48:11 +0530

    span tag and hover effect added

--- components/servicess/service.jsx
+++ components/servicess/service.jsx
@@ -4,22 +4,27 @@ import data from "./data.js";
 
 export default function Service() {
   return (
-    <div className=" text-white bg-[#2a72ef] h-screen">
+    <div className=" text-white bg-[#0b133d] h-screen">
       <div className="container mx-auto">
-        <h1 className="text-4xl font-bold pt-8">Our Services</h1>
+        <h1 className="text-5xl font-bold pt-14 hover:text-blue-400">
+          Our Services
+        </h1>
 
-        <div className="mt-8 p-4 grid grid-cols-3 gap-6 ">
+        <div className="mt-12 p-4 grid grid-cols-3 gap-6 ">
           {data.map((service, index) => (
             <div
               key={index}
-              className="bg-[#0b133d] p-4 h-80 pt-10 flex flex-col justify-center items-center text-center hover:scale-95 rounded-xl"
+              className="border-2 border-[#0b133d] p-4 h-80 pt-10 flex flex-col justify-center items-center text-center hover:scale-95 rounded-xl"
             >
-              <div className="rounded-full bg-[#1c45ca] p-2 w-20 h-20 border-4 border-white">
+              <div className="rounded-full bg-[#2751d8] p-2 w-20 h-20 border-4 border-white">
                 <service.icon className="w-10 h-10 ml-2 mt-2" />
               </div>
 
-              <h2 className="text-3xl font-bold mt-5">{service.name}</h2>
-              <p className="mt-5 text-[20px]">{service.para}</p>
+              <span className="w-1 h-16 bg-white mt-2"></span>
+              <div>
+                <h2 className="text-3xl font-bold mt-5">{service.name}</h2>
+                <p className="mt-5 text-[20px]">{service.para}</p>
+              </div>
             </div>
           ))}
         </div>
