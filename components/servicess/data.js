5a507f8e2ac041196557410f4f4adcbfb993a23f
Viswajith2003 <viswajithviswa715@gmail.com>
2024-04-15 16:03:58 +0530

    alignment seted by using grid

---
+++ components/servicess/data.js
@@ -0,0 +1,34 @@
+import { RiGraduationCapFill } from "react-icons/ri";
+
+const data=[
+    {
+        icon: RiGraduationCapFill,
+        name: "College Events",
+        para: "You can get the latest update of college events"
+    },
+    {
+        icon: RiGraduationCapFill,
+        name: "College Events",
+        para: "You can get the latest update of college events"
+    },
+    {
+        icon: RiGraduationCapFill,
+        name: "College Events",
+        para: "You can get the latest update of college events"
+    },
+    {
+        icon: RiGraduationCapFill,
+        name: "College Events",
+        para: "You can get the latest update of college events"
+    },
+    {
+        icon: RiGraduationCapFill,
+        name: "College Events",
+        para: "You can get the latest update of college events"
+    },
+    {
+        icon: RiGraduationCapFill,
+        name: "College Events",
+        para: "You can get the latest update of college events"
+    },
+]
