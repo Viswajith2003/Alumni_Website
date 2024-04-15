0c5db5820e9975d33d8120022bfcac2bf1184076
Viswajith2003 <viswajithviswa715@gmail.com>
2024-04-15 21:24:56 +0530

    service page mapping completed

--- components/servicess/data.js
+++ components/servicess/data.js
@@ -1,5 +1,9 @@
 import { RiGraduationCapFill } from "react-icons/ri";
 import { IoNotificationsSharp } from "react-icons/io5";
+import { FaUserFriends } from "react-icons/fa";
+import { RiAdminFill } from "react-icons/ri";
+import { FaCalendarCheck } from "react-icons/fa6";
+import { TbBulbFilled } from "react-icons/tb";
 
 const data=[
     {
@@ -13,23 +17,25 @@ const data=[
         para: "You can find the job notification"
     },
     {
-        icon: RiGraduationCapFill,
+        icon: FaUserFriends,
         name: "Connect with Friends",
         para: "You can able to connect with your college friends over here"
     },
     {
-        icon: RiGraduationCapFill,
+        icon: RiAdminFill,
         name: "Admin Panal",
         para: "The website provide an admin panel."
     },
     {
-        icon: RiGraduationCapFill,
+        icon: FaCalendarCheck,
         name: "Only Verified Alumni",
         para: "In this website only verified user are allowed. verificarion is done by admin."
     },
     {
-        icon: RiGraduationCapFill,
+        icon: TbBulbFilled,
         name: "Ideas",
         para: "Alumni can share own views about new technology to others"
     },
 ]
+
+export default data
--- components/servicess/service.jsx
+++ components/servicess/service.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { RiGraduationCapFill } from "react-icons/ri";
+import data from "./data.js";
 
 export default function Service() {
   return (
@@ -7,72 +8,20 @@ export default function Service() {
       <div className="container mx-auto">
         <h1 className="text-4xl font-bold pt-8">Our Services</h1>
 
-        <div className="mt-8 bg-[#26272c] p-4 grid grid-cols-3 gap-6">
-          <div className="bg-[#0b133d] p-4 h-80 pt-10 flex flex-col justify-center items-center text-center ">
-            <div className="rounded-full bg-[#1c45ca] p-2 w-20 h-20 border-4 border-white">
-              <RiGraduationCapFill className="w-10 h-10 ml-2 mt-2" />
+        <div className="mt-8 p-4 grid grid-cols-3 gap-6 ">
+          {data.map((service, index) => (
+            <div
+              key={index}
+              className="bg-[#0b133d] p-4 h-80 pt-10 flex flex-col justify-center items-center text-center hover:scale-95 rounded-xl"
+            >
+              <div className="rounded-full bg-[#1c45ca] p-2 w-20 h-20 border-4 border-white">
+                <service.icon className="w-10 h-10 ml-2 mt-2" />
+              </div>
+
+              <h2 className="text-3xl font-bold mt-5">{service.name}</h2>
+              <p className="mt-5 text-[20px]">{service.para}</p>
             </div>
-
-            <h2 className="text-3xl font-bold mt-5">College Events</h2>
-            <p className="mt-5 text-[20px]">
-              You can get the largest update of the college events.
-            </p>
-          </div>
-
-          <div className="bg-[#0b133d] p-4  h-80 pt-10 flex flex-col justify-center items-center text-center ">
-            <div className="rounded-full bg-[#1c45ca] p-2 w-20 h-20 border-4 border-white">
-              <RiGraduationCapFill className="w-10 h-10 ml-2 mt-2" />
-            </div>
-
-            <h2 className="text-3xl font-bold mt-5">College Events</h2>
-            <p className="mt-5 text-[20px]">
-              You can get the largest update of the college events.
-            </p>
-          </div>
-
-          <div className="bg-[#0b133d] p-4  h-80 pt-10 flex flex-col justify-center items-center text-center ">
-            <div className="rounded-full bg-[#1c45ca] p-2 w-20 h-20 border-4 border-white">
-              <RiGraduationCapFill className="w-10 h-10 ml-2 mt-2" />
-            </div>
-
-            <h2 className="text-3xl font-bold mt-5">College Events</h2>
-            <p className="mt-5 text-[20px]">
-              You can get the largest update of the college events.
-            </p>
-          </div>
-
-          <div className="bg-[#0b133d] p-4 h-80 pt-10 flex flex-col justify-center items-center text-center ">
-            <div className="rounded-full bg-[#1c45ca] p-2 w-20 h-20 border-4 border-white">
-              <RiGraduationCapFill className="w-10 h-10 ml-2 mt-2" />
-            </div>
-
-            <h2 className="text-3xl font-bold mt-5">College Events</h2>
-            <p className="mt-5 text-[20px]">
-              You can get the largest update of the college events.
-            </p>
-          </div>
-
-          <div className="bg-[#0b133d] p-4 h-80 pt-10 flex flex-col justify-center items-center text-center ">
-            <div className="rounded-full bg-[#1c45ca] p-2 w-20 h-20 border-4 border-white">
-              <RiGraduationCapFill className="w-10 h-10 ml-2 mt-2" />
-            </div>
-
-            <h2 className="text-3xl font-bold mt-5">College Events</h2>
-            <p className="mt-5 text-[20px]">
-              You can get the largest update of the college events.
-            </p>
-          </div>
-
-          <div className="bg-[#0b133d] p-4 h-80 pt-10 flex flex-col justify-center items-center text-center ">
-            <div className="rounded-full bg-[#1c45ca] p-2 w-20 h-20 border-4 border-white">
-              <RiGraduationCapFill className="w-10 h-10 ml-2 mt-2" />
-            </div>
-
-            <h2 className="text-3xl font-bold mt-5">College Events</h2>
-            <p className="mt-5 text-[20px]">
-              You can get the largest update of the college events.
-            </p>
-          </div>
+          ))}
         </div>
       </div>
     </div>
