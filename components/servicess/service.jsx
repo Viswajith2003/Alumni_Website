1207739c2cebd360827d5d20eace8aebab610ddf
Viswajith2003 <viswajithviswa715@gmail.com>
2024-04-15 22:01:09 +0530

    set responsive the service pages

--- components/servicess/service.jsx
+++ components/servicess/service.jsx
@@ -17,7 +17,7 @@ export default function Service() {
               className="border-2 border-[#0b133d] p-4 h-80 pt-10 flex flex-col justify-center items-center text-center hover:scale-95 rounded-xl"
             >
               <div className="rounded-full bg-[#2751d8] p-2 w-20 h-20 border-4 border-white mb-2">
-                <RiGraduationCapFill className="w-10 h-10 ml-2 mt-2" />
+                <service.icon className="w-10 h-10 ml-2 mt-2" />
               </div>
               <span className="w-1 h-14 bg-white mt-1"></span>
               <div className="mb-2">
