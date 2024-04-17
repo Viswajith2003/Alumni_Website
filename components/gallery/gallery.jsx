dcd078aa3ab6f9f3966e3aa78333d5eb0bda469d
Viswajith2003 <viswajithviswa715@gmail.com>
2024-04-18 00:23:08 +0530

    Gallary img setted

--- components/gallery/gallery.jsx
+++ components/gallery/gallery.jsx
@@ -6,24 +6,24 @@ export default function gallery() {
       <h1 className="font-bold text-4xl my-5 text-center">Gallery</h1>
       <div className="grid grid-cols-2 w-full">
         <div className="m-4">
-          <div className="border-2 border-black w-auto h-64">
+          <div className=" w-auto h-64">
             <img
-              src="/images/gallary2.jpg"
+              src="/images/gallary8.webp"
               alt="image"
               className="w-[930px] h-64"
             />
           </div>
           <div className="flex gap-4">
-            <div className="border-2 border-black w-full h-60 mt-4">
+            <div className=" w-full h-60 mt-4">
               <img
-                src="/images/gallary3.jpg"
+                src="/images/gallary2.jpg"
                 alt="image"
                 className="w-[930px]"
               />
             </div>
-            <div className="border-2 border-black w-full mt-4">
+            <div className=" w-full mt-4">
               <img
-                src="/images/gallary3.jpg"
+                src="/images/gallary4.webp"
                 alt="image"
                 className="w-[930px]"
               />
@@ -33,24 +33,24 @@ export default function gallery() {
 
         <div className="">
           <div className="flex gap-4">
-            <div className="border-2 border-black w-full h-60 my-4">
+            <div className=" w-full h-60 my-4">
               <img
-                src="/images/gallary3.jpg"
+                src="/images/gallary10.jpg"
                 alt="image"
                 className="w-[930px]"
               />
             </div>
-            <div className="border-2 border-black w-full my-4">
+            <div className=" w-full my-4">
               <img
-                src="/images/gallary3.jpg"
+                src="/images/gallary6.webp"
                 alt="image"
                 className="w-[930px]"
               />
             </div>
           </div>
-          <div className="border-2 border-black w-full h-64">
+          <div className=" w-full h-64">
             <img
-              src="/images/gallary2.jpg"
+              src="/images/gallary5.webp"
               alt="image"
               className="w-[930px] h-64"
             />
@@ -58,23 +58,23 @@ export default function gallery() {
         </div>
       </div>
       <div className="flex gap-4 ml-5 px-2">
-        <div className="border-2 border-black w-full h-72 my-4">
+        <div className=" w-full h-72 my-4">
           <img
-            src="/images/gallary2.jpg"
+            src="/images/gallary7.webp"
             alt="image"
             className="w-[930px] h-72"
           />
         </div>
-        <div className="border-2 border-black w-full my-4">
+        <div className=" w-full my-4">
           <img
-            src="/images/gallary2.jpg"
+            src="/images/gallary1.avif"
             alt="image"
             className="w-[930px] h-72"
           />
         </div>
-        <div className="border-2 border-black w-full my-4">
+        <div className=" w-full my-4">
           <img
-            src="/images/gallary2.jpg"
+            src="/images/gallary9.webp"
             alt="image"
             className="w-[930px] h-72"
           />
