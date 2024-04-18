98acbb8d0e2ab54c0ba70dae45e609122880582a
Viswajith2003 <viswajithviswa715@gmail.com>
2024-04-18 22:34:29 +0530

    almost done

--- app/page.jsx
+++ app/page.jsx
@@ -16,8 +16,8 @@ export default function Home() {
       <Gallery />
       <Contact />
       <Footer />
-      <Register />
-      <Login />
+      {/* <Register />
+      <Login /> */}
     </main>
   );
 }
