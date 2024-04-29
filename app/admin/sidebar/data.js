e83a56f37bc76b2c19fd180e14149314d2893af8
Viswajith2003 <viswajithviswa715@gmail.com>
2024-04-29 08:16:43 +0530

    create a data for fetching

---
+++ app/admin/sidebar/data.js
@@ -0,0 +1,48 @@
+import { AiOutlineDashboard } from "react-icons/ai";
+import { HiUser } from "react-icons/hi";
+import { PiNotepad } from "react-icons/pi";
+import { FaRegMessage } from "react-icons/fa6";
+import { FaUserGraduate } from "react-icons/fa";
+import { IoShareSocialOutline } from "react-icons/io5";
+import { GrGallery } from "react-icons/gr";
+import { MdOutgoingMail } from "react-icons/md";
+import { IoLogOutOutline } from "react-icons/io5";
+
+const data = [
+  {
+    name: "Dashboard",
+    icon: AiOutlineDashboard,
+  },
+  {
+    name: "User",
+    icon: HiUser,
+  },
+  {
+    name: "Add Event",
+    icon: PiNotepad,
+  },
+  {
+    name: "Feedback",
+    icon: FaRegMessage,
+  },
+  {
+    name: "Job",
+    icon: FaUserGraduate,
+  },
+  {
+    name: "Share Idea",
+    icon: IoShareSocialOutline,
+  },
+  {
+    name: "Gallery",
+    icon: GrGallery,
+  },
+  {
+    name: "Send Mail",
+    icon: MdOutgoingMail,
+  },
+  {
+    name: "LogOut",
+    icon: IoLogOutOutline,
+  },
+];
