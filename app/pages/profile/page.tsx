c6b1ec84462755a8859f41ca72be3cb542dd71ba
Viswajith2003 <viswajithviswa715@gmail.com>
2024-05-05 20:52:39 +0530

    form started

--- app/pages/profile/page.tsx
+++ app/pages/profile/page.tsx
@@ -3,11 +3,13 @@ import ProfileIcon from "../../components/profileIcon/page";
 
 function Profile() {
   return (
-    <div className="flex">
-      <div>
+    <div className="flex container mx-auto p-5">
+      <div className="bg-gray-600">
         <ProfileIcon />
       </div>
-      <div className="form">Ith a basic form</div>
+      <div className="form bg-slate-500 p-4">
+        <h1>Personal Details</h1>
+      </div>
     </div>
   );
 }
