6f18e3bf48fe766038bf01dc0def2406ab7121ef
Viswajith2003 <viswajithviswa715@gmail.com>
2024-05-06 21:57:18 +0530

    adjst form div

--- app/pages/profile/page.tsx
+++ app/pages/profile/page.tsx
@@ -5,7 +5,7 @@ import ProfileForm from "../../components/profileform/page";
 function Profile() {
   return (
     <div className="flex container mx-auto p-5 space-x-14 text-white">
-      <div className="bg-gray-600 h-[860px] ml-14">
+      <div className="bg-gray-600 h-[895px] ml-14 rounded-xl">
         <ProfileIcon />
       </div>
       <div className="">
