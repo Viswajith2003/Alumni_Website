4c2450ae69d6a354721253710ec9dc8c98e8f96f
Viswajith2003 <viswajithviswa715@gmail.com>
2024-06-13 11:41:01 +0530

    alumniData page created

---
+++ app/pages/alumniList/data.js
@@ -0,0 +1,38 @@
+const alumniData = [
+  {
+    id: 1,
+    avatar: "https://via.placeholder.com/50",
+    name: "Viswajith",
+    passoutYear: 2020,
+    verified: true,
+  },
+  {
+    id: 1,
+    avatar: "https://via.placeholder.com/50",
+    name: "Jerald",
+    passoutYear: 2020,
+    verified: true,
+  },
+  {
+    id: 1,
+    avatar: "https://via.placeholder.com/50",
+    name: "Aswin",
+    passoutYear: 2020,
+    verified: false,
+  },
+  {
+    id: 1,
+    avatar: "https://via.placeholder.com/50",
+    name: "pinggy",
+    passoutYear: 2020,
+    verified: false,
+  },
+  {
+    id: 1,
+    avatar: "https://via.placeholder.com/50",
+    name: "bhavin",
+    passoutYear: 2020,
+    verified: true,
+  },
+];
+export default alumniData;
--- app/pages/alumniList/page.tsx
+++ app/pages/alumniList/page.tsx
@@ -2,6 +2,7 @@
 
 import React, { useState } from "react";
 import { FaSearch } from "react-icons/fa";
+import alumniData from "./data.js"
 
 const alumniData = [
   {
