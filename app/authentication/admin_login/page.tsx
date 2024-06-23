f45feb7134c2cd79f00d7784e9016d56dd519ad4
Viswajith2003 <viswajithviswa715@gmail.com>
2024-06-23 19:42:04 +0530

    admin login page modified

--- app/authentication/admin_login/page.tsx
+++ app/authentication/admin_login/page.tsx
@@ -6,6 +6,23 @@ import { useRouter } from "next/navigation";
 import Link from "next/link";
 import { Roller } from "react-css-spinners";
 import React from "react";
+import { FaUser, FaLock } from "react-icons/fa"; // Import icons
+import Image from "next/image"; // Import Image component
+import backgroundImg from "../../../public/images/adminLog.jpg"; // Import the background image
+
+// Define the LoadingScreen component
+const LoadingScreen = () => (
+  <div
+    style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh",
+    }}
+  >
+    <Roller color="rgba(6,90,253,1)" size={100} />
+  </div>
+);
 
 export default function Login() {
   // Define the type for errors state
@@ -85,56 +102,70 @@ export default function Login() {
   };
 
   if (isLoading) {
-    return (
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100vh",
-        }}
-      >
-        <Roller color="rgba(6,90,253,1)" size={100} />
-      </div>
-    );
+    return <LoadingScreen />;
   }
 
   return (
-    <div className="bg-[#edeced] h-screen flex justify-center items-center ">
-      <div className="bg-white w-[560px] rounded-xl p-4 flex flex-col justify-center items-center">
-        <div className="flex flex-col justify-center items-center">
-          <h1 className=" font-bold text-3xl mt-5 text-blue-700 ">
-            ADMIN LOGIN
+    <div className="relative h-screen flex justify-center items-center">
+      <Image
+        src={backgroundImg}
+        alt="Background Image"
+        layout="fill"
+        objectFit="cover"
+        quality={100}
+        className="absolute z-0"
+      />
+      <div className="relative w-[560px] rounded-xl p-4 flex flex-col justify-center items-center backdrop-blur-md bg-white bg-opacity-30 z-10">
+        <div className="flex flex-col justify-center items-left">
+          <h1 className="font-bold text-4xl mt-5 text-[#47fbfb] mb-8">
+            Welcome
+            <span className="text-[#47fbfb] ml-3 text-nowrap">Admin!</span>
           </h1>
-          <div className="block w-auto h-auto mb-4 mt-5">
+          <div className="block w-auto h-auto mb-4 mt-5 relative">
+            <FaUser className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500" />
             <input
               type="email"
               placeholder="UserName"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
-              className="border-2 border-black p-3 rounded-xl w-[400px]"
+              className="border-2 border-black p-3 rounded-xl w-[400px] pl-10"
             />
             {errors.email && (
               <p className="text-red-600 text-left">{errors.email}</p>
             )}
           </div>
-          <div className="block w-auto h-auto mb-4">
+          <div className="block w-auto h-auto mb-4 relative">
+            <FaLock className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500" />
             <input
               type="password"
               placeholder="Password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              className="border-2 border-black p-3 rounded-xl w-[400px]"
+              className="border-2 border-black p-3 rounded-xl w-[400px] pl-10"
             />
             {errors.password && (
               <p className="text-red-600 text-left">{errors.password}</p>
             )}
           </div>
         </div>
+        <div className="flex justify-between w-[400px] p-1 mt-3 mb-8">
+          <div className="flex gap-3">
+            <input
+              type="checkbox"
+              className="w-5"
+              checked={rememberMe}
+              onChange={(e) => setRememberMe(e.target.checked)}
+            />
+            <p className="text-[20px] font-semibold">Remember me</p>
+          </div>
+          <p className="text-[16px] font-semibold text-red-600 hover:scale-95 cursor-pointer">
+            Forgot password?
+          </p>
+        </div>
 
         <button
           onClick={handleSubmit}
-          className="mt-5 h-[50px] w-[400px] bg-blue-700 p-2 rounded-xl text-white font-bold mb-1"
+          className="mt-5 h-[50px] w-[400px] bg-blue-700 p-2 rounded-xl text-white font-bold mb-1 hover:scale-95"
         >
           <h1 className="text-2xl font-bold">Login </h1>
         </button>
