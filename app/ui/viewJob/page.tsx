5acfc7ec8fdce4b4d5e2f6eb8d1768abf714ca54
Viswajith2003 <viswajithviswa715@gmail.com>
2024-06-23 17:36:27 +0530

    view job frm another alunmi side design modifed

--- app/ui/viewJob/page.tsx
+++ app/ui/viewJob/page.tsx
@@ -83,13 +83,13 @@ const JobDetails = () => {
   };
 
   return (
-    <div className="bg-[#edeced] pt-4 flex flex-col justify-center items-center text-center h-auto ">
+    <div className="pt-4 flex flex-col justify-center items-center text-center h-auto ">
       <h1 className="text-3xl font-bold mb-5 w-auto">Available Jobs</h1>
-      <div className="container flex flex-row justify-center items-center">
+      <div className="head grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 w-full px-4">
         {jobs.map((job) => (
           <div
             key={job.id}
-            className=" w-auto sm:w-1/2 md:w-1/3 lg:w-1/3 h-auto bg-white p-5 rounded-lg mx-2 my-2"
+            className="w-full h-auto bg-[#dce2f0] p-5 rounded-lg mx-2 my-2"
           >
             <h1 className="text-2xl font-extrabold mb-4 text-center">
               Company Name:
@@ -104,7 +104,7 @@ const JobDetails = () => {
             </p>
             <hr className="mb-3" />
             <hr className="mb-3" />
-            <div className="mb-3 flex flex-col justify-center items-start ml-12">
+            <div className="mb-3 flex flex-col justify-center items-start ml-16 space-y-2">
               <p className="text-lg font-bold flex items-center">
                 <FontAwesomeIcon
                   icon={faBriefcase}
@@ -172,25 +172,23 @@ const JobDetails = () => {
             </div>
 
             {currentUserUID !== job.userId ? (
-              <>
-                <div className="lg:flex flex-row items-center gap-4">
-                  <a
-                    href={job.companyWebsite}
-                    className="bg-blue-700 px-3 py-2 h-10 rounded-lg text-white font-bold hover:border-blue-800 border-2 hover:bg-white hover:text-blue-700 "
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Company Website
-                  </a>
-                  <button className="border-2 border-blue-800 p-1 h-10 w-20 rounded-lg hover:bg-blue-800 hover:text-white text-blue-700">
-                    <h2 className="font-bold">Apply</h2>
-                  </button>
-                </div>
-              </>
+              <div className="lg:flex flex-row items-center gap-4 mt-8 justify-center">
+                <a
+                  href={job.companyWebsite}
+                  className="bg-blue-700 px-3 py-2 h-10 rounded-lg text-white font-bold hover:border-blue-800 border-2 hover:bg-white hover:text-blue-700"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Website
+                </a>
+                <button className="border-2 border-blue-800 p-1 h-10 w-20 rounded-lg hover:bg-blue-800 hover:text-white text-blue-700">
+                  <h2 className="font-bold">Apply</h2>
+                </button>
+              </div>
             ) : (
               <div className="lg:flex flex-row justify-center items-center gap-4 mt-8">
                 <button
-                  className=" p-1 h-10 w-24 rounded-lg bg-green-600 hover:bg-green-700 text-white  hover:text-[15px] hover:scale-95 hover:text-white flex items-center justify-evenly"
+                  className="p-1 h-10 w-24 rounded-lg bg-green-600 hover:bg-green-700 text-white hover:text-[15px] hover:scale-95 hover:text-white flex items-center justify-evenly"
                   onClick={() => handleEdit(job)}
                 >
                   <FontAwesomeIcon icon={faPenToSquare} className="w-6 h-6" />
