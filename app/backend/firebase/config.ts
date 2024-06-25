52ddc9b170fc4e4ee229c89a7a4de416ebc5e075
Jerald-Joyson <jeraldjoyson.inc@gmail.com>
2024-06-25 23:51:37 +0530

    .env file is edited

--- app/backend/firebase/config.ts
+++ app/backend/firebase/config.ts
@@ -7,7 +7,7 @@ import { getStorage } from "firebase/storage";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  apiKey: `${process.env.NEXT_PUBLIC_FIREBASE_API_KEY}NeRkoYjXibmWKRxZ4`,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
